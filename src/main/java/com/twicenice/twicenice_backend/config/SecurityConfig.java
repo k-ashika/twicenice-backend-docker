670fe9933aed63ca1fc5d37505941f5c18b476d9
Ashika K <ashika120110@gmail.com>
2026-03-19 02:22:37 +0530

    Update SecurityConfig.java

--- src/main/java/com/twicenice/twicenice_backend/config/SecurityConfig.java
+++ src/main/java/com/twicenice/twicenice_backend/config/SecurityConfig.java
@@ -63,7 +63,10 @@ public class SecurityConfig {
         http
             .cors(cors -> cors.configurationSource(request -> {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(List.of("http://localhost:4200"));
+                config.setAllowedOrigins(List.of(
+    "http://localhost:4200",
+    "https://twicenice.netlify.app"
+));
                 config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 config.setAllowedHeaders(List.of("*"));
                 config.setAllowCredentials(true);
