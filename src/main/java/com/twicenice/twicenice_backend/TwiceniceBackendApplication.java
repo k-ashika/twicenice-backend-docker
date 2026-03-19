f86827bbba6dee8b0bc50509e111ab279b6f7d1c
Ashika K <ashika120110@gmail.com>
2026-03-20 02:08:28 +0530

    Update TwiceniceBackendApplication.java

--- src/main/java/com/twicenice/twicenice_backend/TwiceniceBackendApplication.java
+++ src/main/java/com/twicenice/twicenice_backend/TwiceniceBackendApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
@@ -17,7 +16,6 @@ public class TwiceniceBackendApplication {
     @Bean
     public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
-
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
@@ -28,12 +26,6 @@ public class TwiceniceBackendApplication {
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
-            }
-
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/api/products/images/**")
-                        .addResourceLocations("file:uploads/");
             }
         };
     }
