c6603b9f505f141eaf6c34b747aaaa710f161c39
Ashika K <ashika120110@gmail.com>
2026-03-20 01:37:50 +0530

    Update AdminOrderController.java

--- src/main/java/com/twicenice/twicenice_backend/controller/AdminOrderController.java
+++ src/main/java/com/twicenice/twicenice_backend/controller/AdminOrderController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin/orders")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {"http://localhost:4200", "https://twicenice.netlify.app"})
 public class AdminOrderController {
 
     @Autowired
