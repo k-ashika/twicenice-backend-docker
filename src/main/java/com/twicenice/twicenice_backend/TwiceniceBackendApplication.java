280bffccaca0f29a67382e6ab297a7d2a6067f7f
Ashika K <ashika120110@gmail.com>
2026-03-21 15:15:53 +0530

    Update TwiceniceBackendApplication.java

--- src/main/java/com/twicenice/twicenice_backend/TwiceniceBackendApplication.java
+++ src/main/java/com/twicenice/twicenice_backend/TwiceniceBackendApplication.java
@@ -3,14 +3,28 @@ package com.twicenice.twicenice_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@RestController
 public class TwiceniceBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TwiceniceBackendApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("TwiceNice Backend is running!");
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
     }
 
     @Bean
