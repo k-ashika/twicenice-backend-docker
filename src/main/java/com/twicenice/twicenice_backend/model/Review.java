5a565b9be78a499837a71b940a1b9d8a6b86268c
Ashika K <ashika120110@gmail.com>
2026-03-22 22:04:53 +0530

    Update Review.java

--- src/main/java/com/twicenice/twicenice_backend/model/Review.java
+++ src/main/java/com/twicenice/twicenice_backend/model/Review.java
@@ -1,67 +1,36 @@
 package com.twicenice.twicenice_backend.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.*;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Review {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private int rating; 
+    private int rating;
     private String comment;
 
     @ManyToOne(fetch = FetchType.LAZY)
-@JoinColumn(name = "product_id")
-@com.fasterxml.jackson.annotation.JsonIgnore
-private Product product;
+    @JoinColumn(name = "product_id")
+    @JsonIgnore
+    private Product product;
+
     private Long userId;
-   
     private String userName;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public int getRating() {
-		return rating;
-	}
-	public void setRating(int rating) {
-		this.rating = rating;
-	}
-	public String getComment() {
-		return comment;
-	}
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-	public Product getProduct() {
-		return product;
-	}
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-	public Long getUserId() {
-		return userId;
-	}
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	
-    
-    
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public int getRating() { return rating; }
+    public void setRating(int rating) { this.rating = rating; }
+    public String getComment() { return comment; }
+    public void setComment(String comment) { this.comment = comment; }
+    public Product getProduct() { return product; }
+    public void setProduct(Product product) { this.product = product; }
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+    public String getUserName() { return userName; }
+    public void setUserName(String userName) { this.userName = userName; }
 }
