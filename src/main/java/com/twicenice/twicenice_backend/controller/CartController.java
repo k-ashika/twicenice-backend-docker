87263ad2c10449906def3886f52b155e1eab5606
Ashika K <ashika120110@gmail.com>
2026-03-22 23:50:16 +0530

    Update CartController.java

--- src/main/java/com/twicenice/twicenice_backend/controller/CartController.java
+++ src/main/java/com/twicenice/twicenice_backend/controller/CartController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/user/cart") 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {"http://localhost:4200", "https://twicenice.netlify.app"})
 public class CartController {
 
     @Autowired
